774eedd8e4568e8e55f8203bfb47813417499168
MaLeX <sgdgdw@ukr.net>
2023-08-02 14:46:13 +0200

    1821

---
+++ 18_21.js
@@ -0,0 +1,26 @@
+//18
+// Passed:The variable product should equal 5.0.
+// Passed:You should use the * operator
+// const product = 2.0 * 2.5;
+
+//19
+// Passed:The variable quotient should equal 2.2
+// Passed:You should use the / operator to divide 4.4 by 2
+// Passed:The quotient variable should only be assigned once
+// const quotient = 4.4 / 2.0; // Change this line
+
+//20
+// Passed:The variable remainder should be initialized
+// Passed:The value of remainder should be 2
+// Passed:You should use the % operator
+const remainder = 11 % 3;
+
+//21
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// Only change code below this line
+// a += 12;
+// b += 9;
+// c += 7;
