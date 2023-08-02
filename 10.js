b14968a2058996a9df9b03d6a4785e11257d892f
MaLeX <sgdgdw@ukr.net>
2023-08-02 14:38:51 +0200

    10th

---
+++ 10.js
@@ -0,0 +1,12 @@
+//10
+// Passed:var should not exist in your code.
+// Passed:You should change fCC to all uppercase.
+// Passed:FCC should be a constant variable declared with const.
+// Passed:The string assigned to FCC should not be changed.
+// Passed:fact should be declared with let.
+// Passed:console.log should be changed to print the FCC and fact variables.
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
