ba0204136f7ebf1d02aac2e9bfc9cef43860b47d
MaLeX <sgdgdw@ukr.net>
2023-08-02 14:47:48 +0200

    2224

---
+++ 22_24.js
@@ -0,0 +1,29 @@
+//22
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// Only change code below this line
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+//23
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+
+// Only change code below this line
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+//24
+// let a = 48;
+// let b = 108;
+// let c = 33;
+
+// // Only change code below this line
+// a /= 12;
+// b /= 4;
+// c /= 11;
