ecf08b48ab089ed96bf9d71752bce2009be9be8e
MaLeX <sgdgdw@ukr.net>
2023-08-02 14:49:00 +0200

    2527

---
+++ 25_27.js
@@ -0,0 +1,9 @@
+//25
+// const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
+
+//26
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//27
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+// console.log(myStr);
