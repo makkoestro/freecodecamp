9241c9f907de45fda5e82407bd13ff7a2a625747
MaLeX <sgdgdw@ukr.net>
2023-08-02 14:42:05 +0200

    1417

---
+++ 14_17.js
@@ -0,0 +1,33 @@
+//14
+// Passed:The variable quotient should be equal to 2.
+// Passed:You should use the / operator.
+const quotient = 66 / 33;
+
+//15
+// Passed:myVar should equal 88.
+// Passed:You should not use the assignment operator.
+// Passed:You should use the ++ operator.
+// Passed:You should not change code above the specified comment.
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//16
+// myVar should equal 10.
+// Passed:myVar = myVar - 1; should be changed.
+// Passed:You should not assign myVar with 10.
+// Passed:You should use the -- operator on myVar.
+// Passed:You should not change code above the specified comment.
+// let myVar = 11;
+
+// Only change code below this line
+// myVar--
+
+//17
+// Passed:myDecimal should be a number.
+// Passed:myDecimal should have a decimal point
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
